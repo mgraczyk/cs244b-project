a22153bfec656c688b5afd3d51bb375f9349ebfb
Michael Graczyk <michael@mgraczyk.com>
2017-12-12 06:43:56 +0000

    Fix node 3 id

--- run_zoo_exp1.sh
+++ run_zoo_exp1.sh
@@ -7,7 +7,7 @@ elif [ $HOSTNAME == 'ip-172-31-13-236' ]; then
 	ZOOCFGDIR=/home/ec2-user/cs244b-project/zoo_exp1/zoo2 \
 		/home/ec2-user/cs244b-project/zookeeper/bin/zkServer.sh start-foreground
 elif [ $HOSTNAME == 'ip-10-81-194-112' ]; then
-	echo "2" > /home/ec2-user/cs244b-project/zoo_exp1/myid
+	echo "3" > /home/ec2-user/cs244b-project/zoo_exp1/myid
 	ZOOCFGDIR=/home/ec2-user/cs244b-project/zoo_exp1/zoo3 \
 		/home/ec2-user/cs244b-project/zookeeper/bin/zkServer.sh start-foreground
 fi
